16a830bb342ec10a8e8a711593332e3d472e9451
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-08-22 13:01:34 -0500

    adds functionality to accept two number inputs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please type a number");
+            var num1 = Console.ReadLine();
+            Console.WriteLine("Please type another number");
+            var num2 = Console.ReadLine();
+            Console.WriteLine($"The numbers you have typed are {num1} and {num2} ");
+
         }
     }
 }
